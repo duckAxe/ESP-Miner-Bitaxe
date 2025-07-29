49b6a06ddd3d21f6d06249092fd0c86de6cffd52
duckAxe <>
2025-07-29 18:54:05 +0200

    Introduction Tooltip Components

--- main/http_server/axe-os/src/app/app.module.ts
+++ main/http_server/axe-os/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { PoolComponent } from './components/pool/pool.component';
 import { NetworkEditComponent } from './components/network-edit/network.edit.component';
 import { HomeComponent } from './components/home/home.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { TooltipIconComponent } from './components/tooltip-icon/tooltip-icon.component';
+import { TooltipTextIconComponent } from './components/tooltip-text-icon/tooltip-text-icon.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { SystemComponent } from './components/system/system.component';
 import { UpdateComponent } from './components/update/update.component';
@@ -40,6 +42,8 @@ const components = [
   NetworkEditComponent,
   HomeComponent,
   ModalComponent,
+  TooltipIconComponent,
+  TooltipTextIconComponent,
   NetworkComponent,
   SettingsComponent,
   LogsComponent,
